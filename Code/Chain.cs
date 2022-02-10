354cf10d9cd90b0dcdfcd7a8553e6ad8cd6ad9be
HighError <hierrorua@gmail.com>
2022-02-10 16:02:19 +0200

    Add Code

---
+++ Code/Chain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GachiBlockchain
+{
+    internal class Chain
+    {
+        public double amount;
+        public string hash;
+        public string prevhash;
+        public int index;
+
+        static public int count = 0;
+
+        public void CreateBlock(double amount, string prevhash)
+        {
+            this.amount = amount;
+            this.prevhash = prevhash;
+            index = count++;
+            hash = GetHash();
+
+        }
+
+        public string GetHash()
+        {
+            MD5 md5Hash = MD5.Create();
+            return GetMd5Hash(md5Hash, amount.ToString() + prevhash + index.ToString());
+
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GachiBlockchain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Chain> blockchain = new List<Chain>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Chain chain = new Chain();
+                chain.CreateBlock(random.NextDouble(), blockchain.Count < 1 ? "" : blockchain.Last().hash);
+                blockchain.Add(chain);
+            }
+
+            if (VerificationBC(blockchain)) Console.WriteLine("Blockchain verified OK");
+            else Console.WriteLine("Blockchain verified ERROR!");
+
+            Console.ReadKey();
+
+        }
+
+        static bool VerificationBC(List<Chain> blockchain)
+        {
+            for (int i = 1; i < blockchain.Count; i++)
+            {
+                if (blockchain[i].hash != blockchain[i].GetHash()) return false;
+                if (blockchain[i].prevhash != blockchain[i-1].hash) return false;
+            }
+            return true;
+        }
+    }
+}
